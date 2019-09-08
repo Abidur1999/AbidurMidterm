fab42aca788f7c4928555b077ed8fd1937efb7ec
Abidur <Abidur1999@gmail.com>
2019-09-08 12:13:21 -0400

    fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,9 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
-
-
+int n1=0,n2=1,n3,i,count=10;
+        System.out.println(n1+""+n2);
+        for (i=2;<count)
     }
+
 }
