84ff4d0f41263d62949a43bbc8e0cf11bfd86a6c
Abidur <Abidur1999@gmail.com>
2019-09-14 01:18:00 -0400

    update

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -24,43 +24,6 @@ public class DataReader<words, word> {
 	 * Demonstrate how to use Stack that includes push,peek,search,pop elements.
 	 * Use For Each loop/while loop/Iterator to retrieve data.
 	 */
-	private String textFile = "/Users/Abidur/Desktop/MidtermJuly2019/src/data/self-driving-car";
-	private Stack<String> stack = new Stack<>();
-	private LinkedList<String> linkedList = new LinkedList<>();
 
-		try (
-	private BufferedReader br = new BufferedReader(new FileReader(textFile))) {
-
-		String line;
-		while (true) {
-			try {
-				if ((line = br.readLine()) == null) break;
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-			for(String word: line.split(" ")) {
-				stack.push(word);
-				linkedList.add(word);
-			}
-
-		}
-
-	} catch (IOException e) {
-		System.err.format("IOException: %s%n", e);
-	}
-
-		System.out.println("First word pushed: " + stack.firstElement());
-		System.out.println("Last word pushed: " + stack.peek());
-
-		System.out.println("\nThe original file content using For Each:");
-		for(
-	private String word: stack)
-			System.out.print(word +"");
-
-		System.out.println("\n\nThe original file content using iterator:");
-	public Iterator<String> iterator = linkedList.iterator();
-		while(iterator.hasNext()) {
-		System.out.print(iterator.next() + " ");
-	}
 }
 
--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -33,13 +33,13 @@ public class LowestNumber {
 		List<String> lowestValue = new ArrayList<String>();
 
 		try {
-			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "SortingNumber");
-			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "SortingNumber");
+			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
+			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//System.out.println(" Data is reading from the Table (tbl_primenumber) and displaying to the console ");
+		//System.out.println(" Data is reading from the Table (tbl_lowestnumber) and displaying to the console ");
 		for(String st:lowestValue) {
 			System.out.println(st);
 
